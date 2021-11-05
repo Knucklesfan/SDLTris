972881f7f62607325d4e9adfd24cd031210118f3
Knucklesfan <bigfootreserchersoftexas@gmail.com>
2021-11-05 11:17:31 -0500

    started netcode implementation

---
+++ source/server.cpp
@@ -0,0 +1,60 @@
+#include "server.h"
+#include <enet/enet.h>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+
+server::server() {
+        // a. Initialize enet
+    if (enet_initialize() != 0) {
+        fprintf(stderr, "An error occured while initializing ENet.\n");
+        exit;
+    }
+    atexit(enet_deinitialize);
+
+    address.host = ENET_HOST_ANY;
+    address.port = 1234;
+}
+void server::start() {
+        srver = enet_host_create(&address, 32, 2, 0, 0);
+
+    if (srver == NULL) {
+        fprintf(stderr, "An error occured while trying to create an ENet server host\n");
+        exit(EXIT_FAILURE);
+    }
+
+    eventStatus = 1;
+
+}
+void server::logic() {
+        eventStatus = enet_host_service(srver, &event, 50000);
+
+        // If we had some event that interested us
+        if (eventStatus > 0) {
+            switch(event.type) {
+                case ENET_EVENT_TYPE_CONNECT: {
+                    printf("(Server) We got a new connection from %x\n",
+                            event.peer->address.host);
+                    break;
+                }
+
+                case ENET_EVENT_TYPE_RECEIVE: {
+                    printf("(Server) Message from client : %s\n", event.packet->data);
+                    // Lets broadcast this message to all
+                    break;
+                }
+                case ENET_EVENT_TYPE_DISCONNECT: {
+                    printf("%s disconnected.\n", event.peer->data);
+
+                    // Reset client's information
+                    event.peer->data = NULL;
+                    break;
+                }
+
+            }
+        }
+
+}
+void server::sendBlockArray(int(blocks)[200]) {
+
+}
---
+++ source/server.h
@@ -0,0 +1,19 @@
+#include <enet/enet.h>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+
+class server {
+    public:
+        ENetAddress address;
+        ENetHost *srver;
+        ENetEvent event;
+        int eventStatus;
+        ENetPacket *packet;
+    
+        void start();
+        void logic();
+        void sendBlockArray(int(blocks)[200]);
+         server();
+    
+};
